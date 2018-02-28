0f99c87bdf35aed90c21f50dc55c28190bde6113
Martin Klapproth <mklapproth@fs-operations.com>
2018-02-28 12:15:12 +0100

    added tpl command

--- dbt.py
+++ dbt.py
@@ -5,7 +5,10 @@ r"""
 """
 
 import argparse
+import json
 import sys
+
+import os
 
 try:
     COMMAND = sys.argv[1].lower().replace("-", "_")
@@ -46,6 +49,38 @@ class DockerBuildTool(object):
     def command_apt_install(self):
         pass
 
+    def command_tpl(self):
+        parser.add_argument('template', metavar='TEMPLATE', type=str)
+        parser.add_argument('output', metavar='OUTPUT', type=str)
+        parser.add_argument('-e', '--env', dest='env_file', type=str, help='path to environment file')
+        parser.add_argument('-j', '--json', dest='json_file', type=str, help='path to json file')
+
+        args = parser.parse_args(ARGS)
+
+        context = os.environ
+
+        if args.env_file:
+            with open(args.env_file) as f:
+                env_contents = f.read()
+            env_extra = [l.strip() for l in env_contents.splitlines() if l.strip()]
+            for line in env_extra:
+                key, value = line.split("=")
+                context[key] = value.strip(' "\'')
+
+        if args.json_file:
+            with open(args.json_file) as f:
+                json_data = json.loads(f.read())
+            context.update(json_data)
+
+        with open(args.template) as f:
+            template_contents = f.read()
+
+        rendered = template_contents % context
+
+        with open(args.output, "w") as f:
+            f.write(rendered)
+
+
     def command_replace(self):
         parser.add_argument('search', metavar='SEARCH', type=str)
         parser.add_argument('replace', metavar='REPLACE', type=str)
@@ -63,7 +98,11 @@ class DockerBuildTool(object):
 
         with open(args.path, 'w') as f:
             f.write(contents)
-        
+
+    def command_install_composer(self):
+        r = os.system('curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer')
+        assert r == 0
+
 if __name__ == '__main__':
     dbt = DockerBuildTool()
     getattr(dbt, "command_" + COMMAND)()
